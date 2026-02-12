591058b8cae48e83ea15c7c1c058e2ce880b0b19
MrTimofeev <nemo.capitan.26ru@mail.ru>
2026-02-12 16:45:32 +0300

    test: Добавлен тест игрока

---
+++ tests/unit/domain/test_player.py
@@ -0,0 +1,32 @@
+from ddd_fantasy_rpg.domain.player import Player, Race, PlayerClass
+from ddd_fantasy_rpg.domain.item import Item, ItemType
+
+def test_player_creation():
+    player = Player(
+        player_id="123",
+        telegram_id=987654321,
+        name="MrSoulKing",
+        race=Race.HUMAN,
+        player_class=PlayerClass.WARRIOR
+    )
+    assert player.level == 1
+    assert player.id == "123"
+    assert "strength" in player.get_total_stats()
+    
+def test_equip_item_requires_level():
+    player = Player("1", 123, "Lowbie", Race.HUMAN, PlayerClass.MAGE)
+    high_level_item = Item(id="sword1", name="Excalibur", item_type= ItemType.WEAPON, level_required=10, rarity=5)
+    
+    try:
+        player.equip_item(high_level_item, "weapon")
+        assert False, "Shoulf have raised ValueError"
+    except ValueError as e:
+        assert "requires level 1" in str(e)
+        
+        
+def test_add_item_to_inventory():
+    player = Player('1', 123, "Looter", Race.ELF, PlayerClass.ROGUE)
+    item = Item(id="dagger", name="Steel Dagger", item_type=ItemType.WEAPON, level_required=1, rarity=2)
+    player.add_item(item)
+    assert len(player.inventory) == 1
+    assert player.inventory[0].name == "Steel Dagger"
