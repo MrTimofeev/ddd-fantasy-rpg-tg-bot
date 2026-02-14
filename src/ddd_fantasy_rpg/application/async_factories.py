5efc94dc2dcd3705382a973e43d4571d8eb74314
MrTimofeev <nemo.capitan.26ru@mail.ru>
2026-02-14 16:56:42 +0300

    feat: добавлена асинхронная фабрика создания use_cases

---
+++ src/ddd_fantasy_rpg/application/async_factories.py
@@ -0,0 +1,32 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from ddd_fantasy_rpg.application import (
+    StartExpeditionUseCase,
+    StartBattleUseCase,
+    CompleteExpeditionUseCase,
+    CompleteBattleUseCase,
+)
+from ddd_fantasy_rpg.infrastructure.repositories import (
+    AsyncSqlitePlayerRepository,
+    AsyncSqliteExpeditionRepository,
+    AsyncSqliteBattleRepository,
+)
+
+
+def create_async_use_cases(session: AsyncSession):
+    player_repo = AsyncSqlitePlayerRepository(session)
+    exp_repo = AsyncSqliteExpeditionRepository(session)
+    battle_repo = AsyncSqliteBattleRepository(session)
+    
+
+    start_battle_uc = StartBattleUseCase(player_repo, battle_repo)
+    start_exp_uc = StartExpeditionUseCase(player_repo, exp_repo)
+    complete_exp_uc = CompleteExpeditionUseCase(
+        exp_repo, player_repo, start_battle_uc)
+    complete_battle_uc = CompleteBattleUseCase(player_repo, battle_repo)
+
+    return {
+        "start_expedition": start_exp_uc,
+        "complete_expedition": complete_exp_uc,
+        "start_battle": start_battle_uc,
+        "complete_battle": complete_battle_uc,
+    }
