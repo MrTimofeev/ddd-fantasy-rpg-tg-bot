8b5138375023586f3bb610b85cccbfa2c1fa9562
MrTimofeev <nemo.capitan.26ru@mail.ru>
2026-02-14 17:00:04 +0300

    feat: добавлен файл запуска бота

---
+++ src/ddd_fantasy_rpg/bot/aiogram_bot/main.py
@@ -0,0 +1,40 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+
+from ddd_fantasy_rpg.bot.aiogram_bot.handlers import register_handlers
+from ddd_fantasy_rpg.infrastructure.database.async_session import get_async_sessionmaker
+from ddd_fantasy_rpg.bot.aiogram_bot.background_tasks import check_completed_expeditions
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+async def main():
+    BOT_TOKEN = ""
+    
+    async_session_maker, init_db = get_async_sessionmaker()
+    
+    await init_db()
+    
+    bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    
+    dp = Dispatcher()
+    
+    # Передаем сессию в middleware
+    dp["async_session_maker"] = async_session_maker
+    
+    register_handlers(dp)
+    
+    
+    # Запуск фоновой задачи
+    asyncio.create_task(check_completed_expeditions(bot, async_session_maker))
+    
+    
+    await dp.start_polling(bot)
+    
+if __name__ == "__main__":
+    asyncio.run(main())
