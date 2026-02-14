69a76e1288ba7972692c7ea889408fd3de5265d2
MrTimofeev <nemo.capitan.26ru@mail.ru>
2026-02-14 17:20:50 +0300

    feat: секреты вынесены в переменные окружения

--- src/ddd_fantasy_rpg/bot/aiogram_bot/main.py
+++ src/ddd_fantasy_rpg/bot/aiogram_bot/main.py
@@ -1,5 +1,8 @@
 import asyncio
 import logging
+import os
+from dotenv import load_dotenv
+
 
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
@@ -13,28 +16,33 @@ from ddd_fantasy_rpg.bot.aiogram_bot.background_tasks import check_completed_exp
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
 async def main():
-    BOT_TOKEN = ""
-    
+
+    load_dotenv()
+    BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+    if not BOT_TOKEN:
+        raise ValueError(
+            "TELEGRAM_BOT_TOKEN is required! Check your .env file.")
+
     async_session_maker, init_db = get_async_sessionmaker()
-    
+
     await init_db()
-    
-    bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-    
+
+    bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(
+        parse_mode=ParseMode.HTML))
+
     dp = Dispatcher()
-    
+
     # Передаем сессию в middleware
     dp["async_session_maker"] = async_session_maker
-    
+
     register_handlers(dp)
-    
-    
+
     # Запуск фоновой задачи
     asyncio.create_task(check_completed_expeditions(bot, async_session_maker))
-    
-    
+
     await dp.start_polling(bot)
-    
+
 if __name__ == "__main__":
     asyncio.run(main())
