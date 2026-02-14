ace625d1b21bb1294b226c0efc3a9bc6519075b9
MrTimofeev <nemo.capitan.26ru@mail.ru>
2026-02-14 16:59:01 +0300

    refactor: добавлены импорты

--- src/ddd_fantasy_rpg/domain/__init__.py
+++ src/ddd_fantasy_rpg/domain/__init__.py
@@ -1,6 +1,7 @@
 from .player import Player, Race, PlayerClass
 from .item import Item, ItemType, ItemStats
 from .moster import Monster
+from .battle import Battle, BattleAction, BattleActionType
 from .expedition import (
     Expedition,
     ExpeditionDistance,
@@ -15,6 +16,7 @@ __all__ = [
     "Player", "Race", "PlayerClass",
     "Item", "ItemType", "ItemStats",
     "Monster",
+    "Battle", "BattleAction", "BattleActionType", 
     "Expedition", "ExpeditionDistance", "ExpeditionEvent",
     "MonsterEncounter", "TraderEncounter", "ResourceGathering", "PlayerDuelEncounter",
 ]
