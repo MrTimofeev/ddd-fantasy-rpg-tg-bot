34730b65bce3d0a6918f095862ebb23701abee92
MrTimofeev <nemo.capitan.26ru@mail.ru>
2026-02-15 16:15:02 +0300

    test: Убран тест на проверку валидности редкости

--- tests/unit/domain/test_item.py
+++ tests/unit/domain/test_item.py
@@ -1,4 +1,4 @@
-from ddd_fantasy_rpg.domain import Item, ItemType, ItemStats
+from ddd_fantasy_rpg.domain import Item, ItemType, ItemStats, Rarity
 
 
 def test_item_creation():
@@ -7,21 +7,8 @@ def test_item_creation():
         name="Iron Sword",
         item_type=ItemType.WEAPON,
         level_required=3,
-        rarity=2,
+        rarity=Rarity.UNCOMMON,
         stats=ItemStats(damage=10, strength=2)
     )
     assert item.name == "Iron Sword"
     assert item.stats.damage == 10
-
-
-def test_invalid_rarity():
-    try:
-        Item(
-            id="x",
-            name="Bad",
-            item_type=ItemType.WEAPON,
-            level_required=1,
-            rarity=6)  # rarity=6
-        assert False
-    except ValueError:
-        pass
