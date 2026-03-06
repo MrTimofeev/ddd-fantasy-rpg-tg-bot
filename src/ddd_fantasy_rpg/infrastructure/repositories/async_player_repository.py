e31680e5fe61c9461cc15d533585a428a72697e8
MrTimofeev <nemo.capitan.26ru@mail.ru>
2026-03-06 18:35:44 +0300

    refactor: изменил запрос к получению игрока по id добавил подргрузку статов, и предметов

--- src/ddd_fantasy_rpg/infrastructure/repositories/async_player_repository.py
+++ src/ddd_fantasy_rpg/infrastructure/repositories/async_player_repository.py
@@ -1,8 +1,9 @@
 from typing import Optional
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
+from sqlalchemy.orm import selectinload
 
-from ddd_fantasy_rpg.domain import Player
+from ddd_fantasy_rpg.domain.player.player import Player
 from ddd_fantasy_rpg.domain.repositories.player_repository import PlayerRepository
 from ddd_fantasy_rpg.infrastructure.database.models import PlayerORM
 from ddd_fantasy_rpg.infrastructure.database.mappers import player_to_orm, player_from_orm
@@ -13,9 +14,21 @@ class AsyncPlayerRepository(PlayerRepository):
         self._session = session
 
     async def get_by_id(self, player_id: str) -> Optional[Player]:
-        result = await self._session.execute(select(PlayerORM).where(PlayerORM.id == player_id))
+        query = (
+            select(PlayerORM)
+            .options(
+                selectinload(PlayerORM.stats),
+                selectinload(PlayerORM.items)
+            )
+            .where(PlayerORM.id == player_id)
+        )
+
+        result = await self._session.execute(query)
         orm = result.scalar_one_or_none()
-        return player_from_orm(orm) if orm else None
+        if not orm:
+            return None
+
+        return player_from_orm(orm)
 
     async def save(self, player: Player) -> None:
         orm = player_to_orm(player)
