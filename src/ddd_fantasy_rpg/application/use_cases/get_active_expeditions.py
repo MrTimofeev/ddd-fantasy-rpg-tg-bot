7ea5c707f957af13fcc6702ef28fb2e8a9f48cd2
MrTimofeev <nemo.capitan.26ru@mail.ru>
2026-03-07 15:44:23 +0300

    refactor: исправил использование uow  в use_case которые используются в фоновых задачах, для поиска нужных экспедиций

--- src/ddd_fantasy_rpg/application/use_cases/get_active_expeditions.py
+++ src/ddd_fantasy_rpg/application/use_cases/get_active_expeditions.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Callable
 
 from ddd_fantasy_rpg.domain.expedition.expedition import Expedition
 from ddd_fantasy_rpg.domain.common.unit_of_work import UnitOfWork
@@ -9,11 +9,17 @@ class GetActiveExpeditionUseCase:
     Используется в фоновых задачах.
     """
     
+    def __init__(
+        self,
+        uow_factory: Callable[[], UnitOfWork],
+    ):
+        self._uow_factory = uow_factory
+ 
    
         
-    async def execute(self, uow: UnitOfWork) -> List[Expedition]:
+    async def execute(self) -> List[Expedition]:
         """
         Возвращает все активные (не завершенные) экспедиции.
         """
-        
-        return await uow.expeditions.get_completed_but_unprocessed_expeditions()
+        async with self._uow_factory() as uow:
+            return await uow.expeditions.get_completed_but_unprocessed_expeditions()
--- src/ddd_fantasy_rpg/application/use_cases/get_completed_expedition.py
+++ src/ddd_fantasy_rpg/application/use_cases/get_completed_expedition.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Callable
 
 from ddd_fantasy_rpg.domain.expedition.expedition import Expedition
 from ddd_fantasy_rpg.domain.common.unit_of_work import UnitOfWork
@@ -9,9 +9,16 @@ class GetCompletedExpeditionUseCase:
     Используется в фоновых задачах.
     """
     
-    async def execute(self, uow: UnitOfWork) -> List[Expedition]:
+    def __init__(
+        self,
+        uow_factory: Callable[[], UnitOfWork],
+
+    ):
+        self._uow_factory = uow_factory
+    
+    async def execute(self) -> List[Expedition]:
         """
         Возвращает все завершенные экспедиции.
         """
-        
-        return await uow.expeditions.get_completed_expedition()
+        async with self._uow_factory() as uow:   
+            return await uow.expeditions.get_completed_expedition()
