de8f5e80d474dc22fe4909b183d91d3bad94af03
MrTimofeev <nemo.capitan.26ru@mail.ru>
2026-03-07 15:43:12 +0300

    refacor: переписал use_case завершения экспедиции

--- src/ddd_fantasy_rpg/application/use_cases/complete_expediton_by_time.py
+++ src/ddd_fantasy_rpg/application/use_cases/complete_expediton_by_time.py
@@ -1,5 +1,8 @@
+from typing import Callable
+
 from ddd_fantasy_rpg.domain.common.unit_of_work import UnitOfWork
 from ddd_fantasy_rpg.domain.common.time_provider import TimeProvider
+from ddd_fantasy_rpg.application.events.dispatcher import EventDispatcher
 from ddd_fantasy_rpg.domain.expedition.exceptions import ExpeditionNotFoundError, ExpeditionNotFinishedError
 
 class CompleteExpeditionByTimeUseCase:
@@ -8,18 +11,31 @@ class CompleteExpeditionByTimeUseCase:
     def __init__(
         self,
         time_provider: TimeProvider,
+        uow_factory: Callable[[], UnitOfWork],
+        dispatcher: EventDispatcher,
+
     ):
         self._time_provider = time_provider
+        self._uow_factory = uow_factory
+        self._dispatcher = dispatcher
        
-    async def execute(self, player_id, uow: UnitOfWork):
-        # 1. Получаем активную вылазку
-        expedition = await uow.expeditions.get_by_player_id(player_id)
-        if not expedition:
-            raise ExpeditionNotFoundError(player_id)
-        if not expedition.is_finished(self._time_provider):
-            raise ExpeditionNotFinishedError()
+    async def execute(self, player_id):
+        async with self._uow_factory() as uow:
+            expedition = await uow.expeditions.get_by_player_id(player_id)
+            if not expedition:
+                raise ExpeditionNotFoundError(player_id)
+            if not expedition.is_finished(self._time_provider.now()):
+                raise ExpeditionNotFinishedError()
+                
+            expedition.complete_travel()
             
-        expedition.complete()
+            await uow.expeditions.save(expedition)
+            
+            events_to_publish = expedition.pop_pending_events()
         
-        await uow.expeditions.save(expedition)
+        if events_to_publish:
+            await self._dispatcher.publish(events_to_publish)
+        
+        return expedition
+            
 
