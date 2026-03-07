8f1b2d898e4334c137106f2e6b5df1a487eb2eb5
MrTimofeev <nemo.capitan.26ru@mail.ru>
2026-03-07 15:40:39 +0300

    feat: добавлен метод уведомление о завершении экспедиции

--- src/ddd_fantasy_rpg/application/events/handlers/telegram_notifier.py
+++ src/ddd_fantasy_rpg/application/events/handlers/telegram_notifier.py
@@ -1,64 +1,100 @@
+from typing import Callable
+
 from ddd_fantasy_rpg.application.events.dispatcher import EventHandler
 from ddd_fantasy_rpg.domain.common.domain_event import DomainEvent
 from ddd_fantasy_rpg.domain.common.unit_of_work import UnitOfWork
 from ddd_fantasy_rpg.domain.player.events import PlayerDied, PlayerCreated
-from ddd_fantasy_rpg.domain.expedition.events import ExpeditionStarted
+from ddd_fantasy_rpg.domain.expedition.events import ExpeditionStarted, ExpeditionCompleted
 
 from ddd_fantasy_rpg.domain.common.notifications import NotificationService
 
+
 class PlayerCreatedTelegramHandler(EventHandler):
-    def __init__(self, notification_service: NotificationService, uow: UnitOfWork):
-        self._uow = uow
-        self._notification_service = notification_service 
-        
+    def __init__(
+        self,
+        notification_service: NotificationService,
+        uow_factory: Callable[[], UnitOfWork],
+    ):
+        self._uow_factory = uow_factory
+        self._notification_service = notification_service
+
     @property
     def subscribed_to(self) -> type[DomainEvent]:
         return PlayerCreated
-    
 
     async def handle(self, event: PlayerCreated) -> None:
-        player = await self._uow.players.get_by_id(event.player_id)
-        if not player:
-            raise ValueError(f"Игрок с {event.player_id} не был найден")
-            
-        
-        await self._notification_service.notify_create_player(player)
+        async with self._uow_factory() as uow:
+            player = await uow.players.get_by_id(event.player_id)
+            if not player:
+                raise ValueError(f"Игрок с {event.player_id} не был найден")
+
+            await self._notification_service.notify_create_player(player)
 
 
 class PlayerDeathTelegramHandler(EventHandler):
-    def __init__(self, notification_service: NotificationService, uow: UnitOfWork):
-        self._uow = uow
-        self._notification_service = notification_service 
-    
+    def __init__(
+        self,
+        notification_service: NotificationService,
+        uow_factory: Callable[[], UnitOfWork],
+    ):
+        self._uow_factory = uow_factory
+        self._notification_service = notification_service
+
     @property
     def subscribed_to(self) -> type[DomainEvent]:
         return PlayerDied
-    
+
     async def handle(self, event: PlayerDied) -> None:
-        player = await self._uow.players.get_by_id(event.player_id)
-        
-        # Вот здесь должен быть вызов метода у у сервиса уведомлений о том что игрок погиб
-        # TODO: нужен реализованный метод уведомления о поражении
-        # грубо гворя что-то тип этого:
-        # await self._notification_service.notify_about_death(player)
-        
-        
+        async with self._uow_factory() as uow:
+            player = await uow.players.get_by_id(event.player_id)
+
+            # Вот здесь должен быть вызов метода у у сервиса уведомлений о том что игрок погиб
+            # TODO: нужен реализованный метод уведомления о поражении
+            # грубо гворя что-то тип этого:
+            # await self._notification_service.notify_about_death(player)
+
 
 class ExpeditionCreatedTelegramHandler(EventHandler):
-    def __init__(self, notification_service: NotificationService, uow: UnitOfWork):
-        self._uow = uow
-        self._notification_service = notification_service 
-    
+    def __init__(
+        self,
+        notification_service: NotificationService,
+        uow_factory: Callable[[], UnitOfWork],
+    ):
+        self._uow_factory = uow_factory
+        self._notification_service = notification_service
+
     @property
     def subscribed_to(self) -> type[DomainEvent]:
         return ExpeditionStarted
 
     async def handle(self, event: ExpeditionStarted) -> None:
-        expedition = await self._uow.expeditions.get_by_player_id(event.player_id)
-        if not expedition:
-            raise ValueError(f"Экспедиция {event.expedition_id} игрока {event.player_id} не была найдена")
-        
-        await self._notification_service.notify_create_expedition(expedition)
-        
-    
- 
+        async with self._uow_factory() as uow:
+            expedition = await uow.expeditions.get_by_player_id(event.player_id)
+            if not expedition:
+                raise ValueError(
+                    f"Экспедиция {event.expedition_id} игрока {event.player_id} не была найдена")
+
+            await self._notification_service.notify_create_expedition(expedition)
+
+
+class ExpeditionCompletedTelegramHandler(EventHandler):
+    def __init__(
+        self,
+        notification_service: NotificationService,
+        uow_factory: Callable[[], UnitOfWork],
+    ):
+        self._uow_factory = uow_factory
+        self._notification_service = notification_service
+
+    @property
+    def subscribed_to(self) -> type[DomainEvent]:
+        return ExpeditionCompleted
+
+    async def handle(self, event: ExpeditionCompleted):
+        async with self._uow_factory() as uow:
+            expedition = await uow.expeditions.get_by_player_id(event.player_id)
+            if not expedition:
+                raise ValueError(
+                    f"Экспедиция {event.expedition_id} игрока {event.player_id} не была найдена")
+
+            await self._notification_service.notify_completed_expedition(expedition)
