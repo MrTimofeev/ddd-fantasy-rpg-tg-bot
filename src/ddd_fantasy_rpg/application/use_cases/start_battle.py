11cb91ba8dbe88900357f3cb77776d6629b0ff7f
MrTimofeev <nemo.capitan.26ru@mail.ru>
2026-03-10 15:15:12 +0300

    redactor: переписал use_case старта боя

--- src/ddd_fantasy_rpg/application/use_cases/start_battle.py
+++ src/ddd_fantasy_rpg/application/use_cases/start_battle.py
@@ -1,6 +1,8 @@
 import uuid
+from typing import Callable
 
 from ddd_fantasy_rpg.domain.common.unit_of_work import UnitOfWork
+from ddd_fantasy_rpg.domain.common.random_provider import RandomProvider
 from ddd_fantasy_rpg.domain.monster.monster import Monster
 from ddd_fantasy_rpg.domain.battle.battle import Battle
 from ddd_fantasy_rpg.domain.player.player import Player
@@ -11,6 +13,7 @@ from ddd_fantasy_rpg.domain.battle.combatant_factory import (
 
 from ddd_fantasy_rpg.domain.player.exceptions import PlayerNotFoundError, PlayerAlreadyInBattleError, SelfDuelError
 from ddd_fantasy_rpg.domain.common.notifications import NotificationService
+from ddd_fantasy_rpg.application.events.dispatcher import EventDispatcher
 
 
 class StartBattleUseCase:
@@ -20,55 +23,69 @@ class StartBattleUseCase:
 
     def __init__(
         self,
+        random_provider: RandomProvider,
+        dispatcher: EventDispatcher,
         notification_service: NotificationService,
+        uow_factory: Callable[[], UnitOfWork],
     ):
+        self._random_provider = random_provider
+        self._dispatcher = dispatcher
         self._notification_service = notification_service
+        self._uow_factory = uow_factory
 
     async def start_pve_battle(
         self,
         player: Player,
         monster: Monster,
-        uow: UnitOfWork,
     ) -> Battle:
         """Запускает PvE бой (игрок против монстра)."""
-        player_combatant = create_combatant_from_player(player)
-        opponent_combatant = create_combatant_from_monster(monster)
-        await self._notification_service.notify_expedition_complete(
-            player_id=player.id,
-            player_hp=player_combatant.current_hp,
-            monster_hp=opponent_combatant.current_hp,
-            monster_level=monster.level,
-            monster_name=monster.name,
-        )
+        async with self._uow_factory() as uow:
+            active_battle = await uow.battles.get_active_battle_for_player(player.id)
+            if active_battle:
+                raise PlayerAlreadyInBattleError(player.id)
 
-        battle_id = str(uuid.uuid4())
+            player_combatant = create_combatant_from_player(player)
+            opponent_combatant = create_combatant_from_monster(monster)
 
-        battle = Battle.start(battle_id, player_combatant, opponent_combatant)
-        await uow.battles.save(battle)
+            battle_id = str(uuid.uuid4())
+
+            battle = Battle.start(
+                battle_id, player_combatant, opponent_combatant, self._random_provider)
+            await uow.battles.save(battle)
+            event_to_publish = battle.pop_pending_events()
+
+        if event_to_publish:
+            await self._dispatcher.publish(event_to_publish)
+
         return battle
 
     async def start_pvp_battle(
         self,
         player1_id: str,
         player2_id: str,
-        uow: UnitOfWork
     ) -> Battle:
         """Запускает PvP дуэль (игрок против игрока)."""
-        player1 = await self._load_player(player1_id, uow)
-        player2 = await self._load_player(player2_id, uow)
+        async with self._uow_factory() as uow:
+            player1 = await self._load_player(player1_id, uow)
+            player2 = await self._load_player(player2_id, uow)
 
-        if player1_id == player2_id:
-            raise SelfDuelError()
+            if player1_id == player2_id:
+                raise SelfDuelError()
 
-        player1_combatant = create_combatant_from_player(player1)
-        player2_combatant = create_combatant_from_player(player2)
+            player1_combatant = create_combatant_from_player(player1)
+            player2_combatant = create_combatant_from_player(player2)
 
-        await self._notification_service.notify_pvp_match_found(player1, player2)
+            battle_id = str(uuid.uuid4())
 
-        battle_id = str(uuid.uuid4())
+            battle = Battle.start(
+                battle_id, player1_combatant, player2_combatant, self._random_provider)
+            await uow.battles.save(battle)
 
-        battle = Battle.start(battle_id, player1_combatant, player2_combatant)
-        await uow.battles.save(battle)
+            event_to_publish = battle.pop_pending_events()
+
+        if event_to_publish:
+            await self._dispatcher.publish(event_to_publish)
+
         return battle
 
     async def _load_player(
